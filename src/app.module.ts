fe9a2f1bf49a1a804799b7a600f8b444433b638e
chetandivekar <chetan6@gmail.com>
2024-07-15 13:43:30 +0530

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    UserAuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule, UserAuthModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        dbName: 'authentication',
+        autoIndex: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user-auth/user-auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserAuthDto } from './dto/create-user-auth.dto';
+import { UpdateUserAuthDto } from './dto/update-user-auth.dto';
+
+@Injectable()
+export class UserAuthService {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create(createUserAuthDto: CreateUserAuthDto) {
+    return 'This action adds a new userAuth';
+  }
+
+  findAll() {
+    return `This action returns all userAuth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} userAuth`;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  update(id: number, updateUserAuthDto: UpdateUserAuthDto) {
+    return `This action updates a #${id} userAuth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} userAuth`;
+  }
+}
